0eceb99efa757bfc7e7e366d576a6be1c2feb6f4
nattabijoanitah <nattabijoanitah563@gmail.com>
2026-06-30 11:34:56 +0300

    added my first react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>My Favorite Things</h1>
+
+      <p>Name: Nattabi Joanitah</p>
+      <p>Favorite Food: Chicken</p>
+      <p>Favorite Hobby: Watching Nigerian Movies</p>
+      <p>Favorite Movie: Heaven Must Wait</p>
+      <p>Dream Career: Software Developer</p>
+    </div>
+  );
+}
+
+export default App;
